2ec838b5ac0b93dc44f055c8c84d73f7e080c6d4
Gary Berkinfand <gberkinfand@gmail.com>
2021-03-31 00:13:58 +0300

    Minter Update

--- frontend/src/components/Minter.js
+++ frontend/src/components/Minter.js
@@ -1,10 +1,7 @@
 import React from "react";
-import Avatar from "@material-ui/core/Avatar";
 import Button from "@material-ui/core/Button";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import TextField from "@material-ui/core/TextField";
-import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
-import Typography from "@material-ui/core/Typography";
 import { makeStyles } from "@material-ui/core/styles";
 import Container from "@material-ui/core/Container";
 
